e4729ca6830b6ec13a84802f1894645bccbcde15
Zach Green <zachjamesgreen@gmail.com>
2021-06-21 17:06:27 -0600

    other stuff

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,16 +8,26 @@ import { MusicService } from './music.service';
 
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
+import { ArtistsComponent } from './artists/artists.component';
+import { AsideComponent } from './aside/aside.component';
+import { NavlinkComponent } from './navlink/navlink.component';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { ArtistcardComponent } from './artistcard/artistcard.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent
+    HomeComponent,
+    ArtistsComponent,
+    AsideComponent,
+    NavlinkComponent,
+    ArtistcardComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     HttpClientModule,
+    FontAwesomeModule,
   ],
   providers: [MusicService],
   bootstrap: [AppComponent]
