1dc27ba1c2d3710d613251224f0003388bd6b885
Zach Green <zachjamesgreen@gmail.com>
2021-06-21 17:04:47 -0600

    pull aside to be component

---
+++ src/app/aside/aside.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+// import { NavlinkComponent } from '../navlink/navlink.component';
+
+@Component({
+  selector: 'app-aside',
+  templateUrl: './aside.component.html',
+  styleUrls: ['./aside.component.scss']
+})
+export class AsideComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
